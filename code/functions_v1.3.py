376e73bb4ef0a51e4acd625421ecf80d454af69e
Dmitriy F <schahmatist@hotmail.com>
2022-04-01 00:26:55 -0400

    FIRST RELEASEgit add .!

--- code/functions_v1.3.py
+++ code/functions_v1.3.py
@@ -6,19 +6,19 @@ import itertools
 
 ########################################################### FUNCTIONS
 
-### FUNCTION FOR LOG TRANSFORMATIONS AND/OR NORMALIZATION 
+### FUNCTION FOR LOG TRANSFORMATIONS AND/OR NORMALIZATION
 
 def log_and_normalize (data, log, norm_type):
     if log == 'log':
-        data = np.log(data) 
+        data = np.log(data)
     if norm_type == 1:
         data = (data-np.mean(data))/np.std(data)  # std normalisation
     elif norm_type == 2:
         data = (data-min(data))/(max(data)-min(data))      #  min_max_min
     elif norm_type == 3:
         data = (data-np.mean(data))/(max(data)-min(data))  # mean norm
-    
-    return data    
+
+    return data
 
 
 
@@ -33,17 +33,17 @@ def hot_encode (data, directions):
 
 def transform_data(x0, y0):
 
-#    cont=[ 'sqft_living','sqft_lot', 'lat','sqft_basement' ] 
-#    nochange=pred[['grade','condition', 'bedrooms', 'renovated','basement','bathrooms']].copy() 
+#    cont=[ 'sqft_living','sqft_lot', 'lat','sqft_basement' ]
+#    nochange=pred[['grade','condition', 'bedrooms', 'renovated','basement','bathrooms']].copy()
 #    nochange=['grade','condition','basement']
-
-    nochange=['basement']
-    log=["sqft_living", 'sqft_lot' ]
-    hot=[ 'grade', 'zipcode', 'waterfront','view', 'yr_built']
 
 #    ord_cat=pd.DataFrame([])
 #    ord_cat=pd.DataFrame(x0['view2'].cat.codes)
 #    ord_cat.columns=['view']
+
+    nochange=['basement','grade']
+    log=["sqft_living", 'sqft_lot' ]
+    hot=[ 'zipcode', 'view', 'yr_built', 'waterfront']
 
     asis=x0[nochange]
 
@@ -66,18 +66,21 @@ def transform_data(x0, y0):
 
     return x1,y1
 
-
 ### TO GET COEFFECIENTS FROM MODEL OBJECT
 
-
-def get_coeff( year, zipcode, grade, waterfront, view, coef_df):
+def get_coeff( year,zipcode,grade, water,view, coef_df):
     intercept=coef_df[coef_df['Column'] == "const"]["Value"].tolist()[0]
+    sqft_living_coef=coef_df[coef_df['Column'] == "sqft_living"]["Value"].tolist()[0]
+    sqft_lot_coef=coef_df[coef_df['Column'] == "sqft_lot"]["Value"].tolist()[0]
+    basement_coef=coef_df[coef_df['Column'] == "basement"]["Value"].tolist()[0]
+    grade_coef=coef_df[coef_df['Column'] == "grade"]["Value"].tolist()[0]
 
-    if waterfront == 'WATERFRONT':
+    if water == 1:
         water_coef=coef_df[coef_df['Column'] == "wat_YES"]["Value"].tolist()[0]
     else:
-        water_coef=0        
- 
+        water_coef=0
+
+
     if len(str(year)) != 4:
         year_coef=0
     else:
@@ -89,32 +92,13 @@ def get_coeff( year, zipcode, grade, waterfront, view, coef_df):
     else:
         try: zipcode_coef=coef_df[coef_df['Column'].str.endswith(str(zipcode))]["Value"].tolist()[0]
         except: zipcode_coef=0
-    
-#    grade_coef=coef_df[coef_df['Column'] == "grade"]["Value"].tolist()[0]
 
-    if len(str(grade))!=1 and len(str(grade))!=2:
-        grade_coef=0
-    else:
-        try: grade_coef=coef_df[coef_df['Column'].str.endswith('gra_'+str(grade))]["Value"].tolist()[0]
-        except: grade_coef=0
-    
-
-    if view  not in ['NONE', 'FAIR','GOOD','EXCELLENT']:
+    if view  not in ['NONE','FAIR','GOOD','EXCELLENT']:
         view_coef=0
     else:
         try: view_coef=coef_df[coef_df['Column'].str.endswith(view)]["Value"].tolist()[0]
         except: view_coef=0
-    
-#    sqft_lot_coef=coef_df[coef_df['Column'] == "sqft_lot"]["Value"].tolist()[0]
-    try:
-        sqft_lot_coef=coef_df[coef_df['Column'] == "sqft_lot"]["Value"].tolist()[0]
-    except:
-        sqft_lot_coef=0
 
-    sqft_living_coef=coef_df[coef_df['Column'] == "sqft_living"]["Value"].tolist()[0]
-    basement_coef=coef_df[coef_df['Column'] == "basement"]["Value"].tolist()[0]
-   
-    return intercept, sqft_living_coef, sqft_lot_coef, basement_coef, year_coef, zipcode_coef, grade_coef,  water_coef, view_coef 
-
+    return intercept, sqft_living_coef, sqft_lot_coef, basement_coef, water_coef, grade_coef, year_coef, zipcode_coef, view_coef
 
 
